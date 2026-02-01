bd469dad1146ae3d6aa28cb2a1af8a2e8d5ffa99
James McDonald <jamesmcdonald112@yahoo.ie>
2026-02-01 17:25:21 +0000

    chore(brand): update logo, firm config, and design tokens

--- src/config/firm.ts
+++ src/config/firm.ts
@@ -2,6 +2,6 @@ export const firm = {
 	companyName: "Mary Molloy Solicitors",
 	county: "Co. Kilkenny",
 	phoneDisplay: "(056) 776 5829",
-	phoneHref: "tel:+353567765829",
+	phoneHref: "+353567765829",
 	address: "2 Rose Inn St, Dukesmeadows, Kilkenny, R95 W58D",
 };
